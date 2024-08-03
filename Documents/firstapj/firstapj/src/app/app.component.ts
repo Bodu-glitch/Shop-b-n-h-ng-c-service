dea1b4c0f2b43cf13f2de11c37fa759087611b49
thanhhuy5902 <tranthanhhuy42@gmail.com>
2024-08-03 19:28:16 +0700

    3/8/2024

--- Documents/firstapj/firstapj/src/app/app.component.ts
+++ Documents/firstapj/firstapj/src/app/app.component.ts
@@ -1,25 +1,30 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-export interface Item {
-  id :number;
-  name: String;
-  quantity: number;
-  price: number;
-  inStock :number;
-  img :string;
-}
+import {NgFor} from '@angular/common';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterOutlet} from '@angular/router';
+import {NavbarComponent} from "./components/navbar/navbar.component";
+import {CardComponent} from "./components/card/card.component";
+import {ProductModel} from "./models/product.model";
+import {createWebpackLoggingCallback} from "@angular-devkit/build-angular/src/tools/webpack/utils/stats";
+import {StoreService} from "./services/store.service";
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, NgFor],
+  imports: [RouterOutlet, FormsModule, NgFor, NavbarComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
   title = "hello"
+
+  constructor(public storeService: StoreService) {
+    console.log(this.storeService.products)
+  }
+
+
   // stuff = ["đi chợ", "Mua đồ ăn", "Học Bài","Đi ngủ"];
   // completedStuff: string[] = [];
 
@@ -39,65 +44,8 @@ export class AppComponent {
   // }
 
 
-  obj1 = { id: 1, name: 'món đồ 1', quantity: 0, price: 100000 ,inStock:10, img: '../assets/1.1.png' } as Item;
-  obj2 = { id: 2, name: 'món đồ 2', quantity: 0, price: 200000 ,inStock:10,img: '../assets/1.1.png' } as Item;
-  obj3 = { id: 3, name: 'món đồ 3', quantity: 0, price: 300000 ,inStock:10,img: '../assets/1.1.png' } as Item;
+  // addtocart(item: any) {
+  //
+  // }
 
-
-  total :number = 0;
-  products: Item[] = [this.obj1, this.obj2, this.obj3]
-  cart: Item[] = []
-
-
-
-  addToCart(index: number) {
-    let findIndex = this.cart.findIndex((element) => {
-      return element.id == this.products[index].id;
-    }); // Đi tìm trong giỏ hàng có tồn tại sp mà mình muốn thêm hay không
-    if (findIndex != -1) {// Nếu tồn tại (index != -1)
-      this.cart[findIndex].quantity += 1;
-      if(this.products[index].inStock <= 0){
-        return;
-      }else{
-        this.products[index].inStock--;
-      } // Tăng số lượng lên 1
-    } else {// Nếu không tồn tại
-      this.cart.push({// Thêm sp mới đó vào
-        id: this.products[index].id,
-        name: this.products[index].name,
-        price: this.products[index].price,
-        inStock : this.products[index].price,
-        quantity: 1,
-        img: '../assets/1.1',
-      });
-      this.products[index].inStock--;
-    }
-    console.log(this.cart);
-    this.totalcost();
-  }
-
-  deletefromCart (index:number){
-    let findIndex:any = this.products.find((element) => {
-      return element.id == this.cart[index].id;
-    }); 
-    
-    if (this.cart[index].quantity > 0){
-      this.cart[index].quantity--;
-      findIndex.inStock ++;
-    }
-
-    if(this.cart[index].quantity == 0){
-      this.cart.splice(index,1);
-    }
-  }
-
-  totalcost (){
-    this.total = 0;
-
-    for (let i = 0; i<this.cart.length;i++){
-      this.total = this.total + this.cart[i].price * this.cart[i].quantity;
-    }
-    
-    // this.cart.splice(0,this.cart.length)
-  }
 }
---
+++ Documents/firstapj/firstapj/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {SearchbarComponent} from "./components/searchbar/searchbar.component";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    SearchbarComponent
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
